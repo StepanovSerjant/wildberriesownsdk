cfc4a2848cdf3559121e58846de31faa22e9b52b
astepanov2 <astepanov@emphasoft.com>
2024-11-02 14:29:48 +0300

    changed setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-__version__ = 'dev'
+__version__ = "0.0.1"
 
 setup(
-    name="wbapi-own-sdk",
+    name="wildberriesbapi",
     version=__version__,
     description="Wildberries API SDK. (not official)",
-    url="https://github.com/StepanovSerjant/wildberries-own-sdk",
+    url="https://github.com/StepanovSerjant/wildberriesownsdk",
     author="Aleksey Stepanov",
     author_email="stpnvlks@gmail.com",
+    py_modules=["wbapi"],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
