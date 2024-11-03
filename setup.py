d68a4984d278aa3fb9c9fa516e9d639f4e7815c6
astepanov2 <astepanov@emphasoft.com>
2024-11-03 14:21:14 +0300

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+from os.path import join, dirname
 from setuptools import setup
 
 __version__ = "0.0.1"
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 
 setup(
     name="wildberriesbapi",
     version=__version__,
     description="Wildberries API SDK. (not official)",
+    long_description=open(join(dirname(__file__), "README.txt")).read(),
     url="https://github.com/StepanovSerjant/wildberriesownsdk",
     author="Aleksey Stepanov",
     author_email="stpnvlks@gmail.com",
     py_modules=["wbapi"],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
