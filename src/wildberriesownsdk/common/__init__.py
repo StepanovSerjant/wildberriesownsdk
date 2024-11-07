404e46c47a239717a00c78b62db8ee0f963a6590
astepanov2 <astepanov@emphasoft.com>
2024-11-07 17:46:42 +0300

    updates

---
+++ src/wildberriesownsdk/common/__init__.py
@@ -0,0 +1,7 @@
+all = [
+    "APIKeyIntrospectionException",
+    "GettingDataFromAPIException"
+]
+
+from .exceptions import GettingDataFromAPIException
+from .exceptions import APIKeyIntrospectionException
