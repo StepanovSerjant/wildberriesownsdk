e20e06821204ad968170ccef8e2b9edb15a7ca62
astepanov2 <astepanov@emphasoft.com>
2025-02-12 19:57:06 +0300

    refactor: deleted extra service logic

--- src/wildberriesownsdk/api/services.py
+++ src/wildberriesownsdk/api/services.py
@@ -5,15 +5,8 @@ class RequestService:
     timeout = httpx.Timeout(15.0, connect=30)
 
     def request(self, *args, **kwargs) -> httpx.Response:
-        request_kwargs = self.update_kwargs_with_timeout(kwargs)
-        return httpx.request(*args, **request_kwargs)
+        return httpx.request(*args, **kwargs)
 
     async def async_request(self, *args, **kwargs) -> httpx.Response:
-        request_kwargs = self.update_kwargs_with_timeout(kwargs)
         async with httpx.AsyncClient() as client:
-            return await client.request(*args, **request_kwargs)
-
-    @classmethod
-    def update_kwargs_with_timeout(cls, request_kwargs: dict) -> dict:
-        request_kwargs.setdefault("timeout", cls.timeout)
-        return request_kwargs
+            return await client.request(*args, **kwargs)
